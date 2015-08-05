5ddcd083ece4583ad915b3bb2dd36f9063df5137
xslidian <xslidian@gmail.com>
2015-08-05 19:42:01 +0800

    use radio buttons instead of select-option

--- options.js
+++ options.js
@@ -4,22 +4,49 @@ var fillselect = function(e, kv, subkey) {
 	e.innerHTML = '';
 	for (var k in kv) {
 		if (kv.hasOwnProperty(k)) {
-			var o = document.createElement('option');
+			var l = document.createElement('label');
+			var o = document.createElement('input');
+			o.setAttribute('type', 'radio');
+			o.setAttribute('name', 'r_' + e.id);
 			o.setAttribute('value', k);
-			o.innerText = subkey ? kv[k][subkey] : kv[k];
-			e.appendChild(o);
+			l.appendChild(o);
+			var t = document.createTextNode( subkey ? kv[k][subkey] : kv[k] );
+			l.appendChild(t);
+			var br = document.createElement('br');
+			l.appendChild(br);
+			e.appendChild(l);
 		}
 	}
 };
 
+var radioget = function(name) {
+	var r = document.getElementsByName(name);
+	for(var i = 0; i < r.length; i++) {
+		if(r[i] && r[i].checked) return r[i].value;
+	}
+	return null;
+};
+
+var radioset = function(name, val) {
+	var r = document.getElementsByName(name);
+	for(var i = 0; i < r.length; i++) {
+		if(r[i] && r[i].value == val) {
+			r[i].checked = true;
+			r[i].setAttribute('checked', true);
+			return r[i].value;
+		}
+	}
+	return false;
+};
+
 s_site.onchange = function() {
-	fillselect(s_dict, sitedicts[s_site.value], 'desc');
+	fillselect(s_dict, sitedicts[radioget('r_s_site')], 'desc');
 };
 
 b_save.onclick = function() {
 	chrome.storage.sync.set({
-		site: s_site.value,
-		dict: s_dict.value
+		site: radioget('r_s_site'),
+		dict: radioget('r_s_dict')
 	}, function(){
 		msg.innerText = 'Saved.';
 		msg.display = 'block';
@@ -37,10 +64,10 @@ fillselect(s_site, sitenames);
 chrome.storage.sync.get(vars, function(r){
 	console.log(vars, r);
 	vars = r;
-	s_site.value = vars.site;
+	radioset('r_s_site', vars.site);
 	s_site.onchange();
 	setTimeout(function(){
-		s_dict.value = vars.dict;
+		radioset('r_s_dict', vars.dict);
 	}, 200);
 });
 
